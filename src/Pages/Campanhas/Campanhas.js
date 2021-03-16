39e687822c86fec4221bc5ac8eda5b7b48be6b30
Átila <61899222+Atila-Aguiar@users.noreply.github.com>
2021-03-16 18:32:48 -0300

    Update Campanhas.js

--- src/Pages/Campanhas/Campanhas.js
+++ src/Pages/Campanhas/Campanhas.js
@@ -26,7 +26,7 @@ export default function Campanhas () {
 
 
     useState( async () => {
-    const answer = await fetch("https://back-livro-corrente.herokuapp.com/inativos",{
+    const answer = await fetch("https://back-livro-corrente.herokuapp.com/campanha",{
         method: "POST",
         headers:{"Content-Type":"application/json"},
         body: JSON.stringify({"id":NumB})
